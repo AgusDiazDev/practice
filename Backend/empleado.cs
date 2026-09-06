b39125962fbc6ff2b67d2ddb3a8e5aac30c7211d
Agustin Diaz <agustindiazvigna@gmail.com>
2026-09-05 21:54:36 -0300

    fix git

---
+++ Backend/empleado.cs
@@ -0,0 +1,8 @@
+public class Empleado
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Departamento { get; set; } = string.Empty;
+    public decimal Salario { get; set; }
+    public bool Activo { get; set; }
+}
